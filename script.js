9d3aea29ec21074b11ad5f776daa46711c2b6cb5
pablo <pepe150@hotmail.es>
2025-12-05 23:25:37 -0500

    archivos agregados

---
+++ script.js
@@ -0,0 +1,4 @@
+// Si necesitas alguna interacción, como mostrar un mensaje de bienvenida al iniciar sesión o registro.
+document.querySelector('.btn').addEventListener('click', function() {
+  alert("¡Bienvenido a NeoBarber!");
+});
